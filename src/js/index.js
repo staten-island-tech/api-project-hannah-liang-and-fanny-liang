0a07f428eaab473c33ac1d92f804146e274b29c4
fannyl6720 <fanny.liang23@sitechhs.com>
2021-05-04 12:49:29 -0400

    start

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,21 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+
+
+
+const query = async function () {
+    try {
+        const response = await fetch(''
+    );
+    const data = await response.json();
+    data.results.forEach((books) => {
+        DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend", 
+            );
+    });
+} catch (error) {
+    alert("Hey something went wrong");
+}
+};
+query();
