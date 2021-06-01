ca2fbf40a91c4e8e6a1f4549cae5ae9f9d17e3f8
hannahliang11 <71453030+hannahliang11@users.noreply.github.com>
2021-06-01 11:32:45 -0400

    working

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import { genres } from "./genre";
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/discover/tv?api_key=283bc35e6dd7de4b871d46535cba89fb&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate${key}`
+      `https://api.themoviedb.org/3/discover/tv?api_key=283bc35e6dd7de4b871d46535cba89fb&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`
     );
     const data = await response.json();
     data.results.forEach((show) => {
